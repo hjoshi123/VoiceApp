876a3ef686cd1d952dd208861422194d12b08902
Aditya Gupta <adi.gupta13@gmail.com>
2018-02-17 23:47:49 +0530

    Added keyboard Listener

--- app/src/main/java/edu/msrit/csrlearn/MainActivity.java
+++ app/src/main/java/edu/msrit/csrlearn/MainActivity.java
@@ -2,12 +2,37 @@ package edu.msrit.csrlearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toast toast;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearLayout = findViewById(R.id.linear_layout);
+        toast = Toast.makeText(this, "Yolo", Toast.LENGTH_SHORT);
+        Log.d("oncreate", "works");
+
+        linearLayout.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                Log.d("Key", i + " " + keyEvent.toString() );
+                return false;
+            }
+
+
+        });
+
+
     }
+
+
 }
