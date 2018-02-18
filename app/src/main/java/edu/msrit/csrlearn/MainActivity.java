f49fe4cf21e23aededcca48c97f2724ce9e299b3
Hemant Joshi <hemant2612@hotmail.com>
2018-02-18 12:06:05 +0530

    Audio Manager

--- app/src/main/java/edu/msrit/csrlearn/MainActivity.java
+++ app/src/main/java/edu/msrit/csrlearn/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.msrit.csrlearn;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.KeyListener;
@@ -11,7 +14,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
+    private LinearLayout linearLayout;
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    private AudioManager.OnAudioFocusChangeListener mAudioChangeListner =
+            new AudioManager.OnAudioFocusChangeListener(){
+                @Override
+                public void onAudioFocusChange(int focusChange){
+                    if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                        mMediaPlayer.pause();
+                        mMediaPlayer.seekTo(0);
+                    }
+                    else if(focusChange == AudioManager.AUDIOFOCUS_GAIN)
+                        //we gained audiofocus so we have to start playing the song
+                        mMediaPlayer.start();
+                    else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                        //we lost the audiofocus so release the mediaplayer resources
+                        releaseMediaPlayer();
+                    }
+                }
+            };
+
+    //releasing media resources
+    private MediaPlayer.OnCompletionListener mMediaOnCompletionListner = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +50,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         linearLayout = findViewById(R.id.linear_layout);
 
-
-//        linearLayout.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-//                makeToast(keyEvent);
-//                Log.d("Key", i + " " + keyEvent.toString() );
-//                return false;
-//            }
-//
-//        });
-
-
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        makeToast(event);
-        return true;
+        switch (keyCode){
+            case KeyEvent.KEYCODE_ENTER:
+                Log.d("MainActivity","Enter was pressed");
+                playAudio();
+                return true;
+        }
+        return false;
     }
 
+    private void playAudio() {
+        releaseMediaPlayer();
+        int result = mAudioManager.requestAudioFocus(mAudioChangeListner,
+                AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
 
-    public void makeToast(KeyEvent keyEvent) {
-        Toast.makeText(this, keyEvent.toString(), Toast.LENGTH_SHORT).show();
+        if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
+            mMediaPlayer = MediaPlayer.create(this,R.raw.game_of_thrones);
+            mMediaPlayer.start();
+
+            mMediaPlayer.setOnCompletionListener(mMediaOnCompletionListner);
+
+        }
     }
 
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
 
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mAudioChangeListner);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
 }
